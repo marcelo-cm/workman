02e9c13126d63308b4ec71fec662f8d9a4771dde
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-06-16 18:21:12 -0400

    last type stuff

--- classes/User.ts
+++ classes/User.ts
@@ -9,7 +9,7 @@ export class User {
   ignore_label_id: string;
   scanned_label_id: string;
   gmail_integration_status: string;
-  quickbooks_intergation_status: string;
+  quickbooks_integration_status: string;
 
   constructor({
     id,
@@ -17,13 +17,13 @@ export class User {
     ignore_label_id,
     scanned_label_id,
     gmail_integration_status,
-    quickbooks_intergation_status,
+    quickbooks_integration_status,
   }: User) {
     this.id = id;
     this.created_at = created_at;
     this.ignore_label_id = ignore_label_id;
     this.scanned_label_id = scanned_label_id;
     this.gmail_integration_status = gmail_integration_status;
-    this.quickbooks_intergation_status = quickbooks_intergation_status;
+    this.quickbooks_integration_status = quickbooks_integration_status;
   }
 }
