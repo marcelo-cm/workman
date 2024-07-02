552cec6c0c34b8fa48f8595a72237b779c5b9777
ehan7 <han.ethan.07@gmail.com>
2024-07-02 15:58:39 -0400

    added drag files functionality

--- components/dashboards/UploadFileButton.tsx
+++ components/dashboards/UploadFileButton.tsx
@@ -12,6 +12,7 @@ import PDFSplitter from '../PDF/PDFSplitter/PDFSplitter';
 const UploadFileButton = () => {
   const fileInputRef = useRef<null | HTMLInputElement>(null);
   const [filesToUpload, setFilesToUpload] = useState<File[]>([]);
+  const [isDragActive, setDragActive] = useState(false);
 
   const handleButtonClick = () => {
     if (fileInputRef.current) {
@@ -26,6 +27,28 @@ const UploadFileButton = () => {
     }
 
     const files = Array.from(filesList) as File[];
+
+    setFilesToUpload((prevFiles) => [...prevFiles, ...files]);
+  };
+
+  const handleDragEnter = (event: React.DragEvent) => {
+    event.preventDefault();
+    setDragActive(true);
+  };
+
+  const handleDragLeave = (event: React.DragEvent) => {
+    event.preventDefault();
+    setDragActive(false);
+  };
+
+  const handleDragOver = (event: React.DragEvent) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event: React.DragEvent) => {
+    event.preventDefault();
+    setDragActive(false);
+    const files = Array.from(event.dataTransfer.files) as File[];
 
     setFilesToUpload((prevFiles) => [...prevFiles, ...files]);
   };
@@ -49,6 +72,10 @@ const UploadFileButton = () => {
             <button
               className="flex h-full w-full items-center justify-center rounded-lg border border-dashed border-wm-white-500 bg-wm-white-50 text-wm-white-700 hover:border-wm-orange-500 hover:bg-wm-orange-50 hover:text-wm-orange-700"
               onClick={handleButtonClick}
+              onDragEnter={handleDragEnter}
+              onDragLeave={handleDragLeave}
+              onDragOver={handleDragOver}
+              onDrop={handleDrop}
             >
               Drag Files or Click to Upload
             </button>
