f26b6d92e09b8662dc1e8717e1f7e81c1124825a
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-07-29 20:50:21 -0400

    uploaded invoice table.

--- components/(dashboards)/bills/extraction/(tabs)/upload/UploadToQuickBooks.tsx
+++ components/(dashboards)/bills/extraction/(tabs)/upload/UploadToQuickBooks.tsx
@@ -1,40 +1,25 @@
 import React, { useEffect, useState } from 'react';
 
-import {
-  ArrowLeftIcon,
-  BookmarkIcon,
-  EyeOpenIcon,
-} from '@radix-ui/react-icons';
-import { HammerIcon, Loader2Icon } from 'lucide-react';
+import { ArrowLeftIcon } from '@radix-ui/react-icons';
+import { HammerIcon } from 'lucide-react';
 
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
 import { ComboBox } from '@/components/ui/combo-box';
 import LoadingState from '@/components/ui/empty-state';
 import IfElseRender from '@/components/ui/if-else-renderer';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
 import { TabsList, TabsTrigger } from '@/components/ui/tabs';
 
-import { InvoiceStatus } from '@/constants/enums';
 import { Invoice_Quickbooks, Vendor } from '@/interfaces/quickbooks.interfaces';
 import Invoice from '@/models/Invoice';
 
 import { useExtractionReview } from '../../ExtractionReview';
+import InvoiceDataTable from './InvoiceDataTable';
 
 const UploadToQuickBooks = () => {
   const { files, accounts, vendors, customers, activeIndex, setActiveIndex } =
     useExtractionReview();
   const [uploadedFileIndexes, setUploadedFileIndexes] = useState<number[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
   const [transformedFiles, setTransformedFiles] = useState<
     Invoice_Quickbooks[]
   >([]);
@@ -60,13 +45,6 @@ const UploadToQuickBooks = () => {
     }
   }, [uploadedFileIndexes]);
 
-  async function uploadToQuickBooks(fileIndex: number) {
-    setIsLoading(true);
-    await Invoice.uploadToQuickbooks(transformedFiles[fileIndex]);
-    setUploadedFileIndexes([...uploadedFileIndexes, fileIndex]);
-    setIsLoading(false);
-  }
-
   const transformData = async () => {
     const transformed: Invoice_Quickbooks[] = await Promise.all(
       files.map(async (file) => {
@@ -83,36 +61,6 @@ const UploadToQuickBooks = () => {
     setTransformedFiles(updatedFiles);
   };
 
-  const handleCustomerSelect = (
-    value: { sparse: boolean; Id: string; DisplayName: string },
-    fileIndex: number,
-    lineItemIndex: number,
-  ) => {
-    const updatedFiles = [...transformedFiles];
-    updatedFiles[fileIndex].data.lineItems[lineItemIndex].customerId = value.Id;
-    setTransformedFiles(updatedFiles);
-  };
-
-  const handleBillableChange = (
-    value: boolean,
-    fileIndex: number,
-    lineItemIndex: number,
-  ) => {
-    const updatedFiles = [...transformedFiles];
-    updatedFiles[fileIndex].data.lineItems[lineItemIndex].billable = value;
-    setTransformedFiles(updatedFiles);
-  };
-
-  const handleAccountSelect = (
-    value: { sparse: boolean; Id: string; Name: string },
-    fileIndex: number,
-    lineItemIndex: number,
-  ) => {
-    const updatedFiles = [...transformedFiles];
-    updatedFiles[fileIndex].data.lineItems[lineItemIndex].accountId = value.Id;
-    setTransformedFiles(updatedFiles);
-  };
-
   return (
     <>
       <div className="flex h-full flex-col gap-4 p-4 ">
@@ -120,7 +68,7 @@ const UploadToQuickBooks = () => {
           condition={initialLoading}
           ifTrue={<LoadingState />}
           ifFalse={files.map((file, fileIndex) => (
-            <div>
+            <div key={file.id}>
               <IfElseRender
                 condition={activeIndex == fileIndex}
                 ifTrue={
@@ -129,10 +77,8 @@ const UploadToQuickBooks = () => {
                   </div>
                 }
               />
-
               <div
                 className={`space-y-3 rounded-md border  ${activeIndex == fileIndex ? 'border-l-2 border-l-wm-orange-500 rounded-t-none border-t-0' : null}`}
-                key={fileIndex + file.id}
               >
                 <div
                   className={`flex w-full items-center justify-between border-b p-2 text-sm font-medium `}
@@ -168,149 +114,14 @@ const UploadToQuickBooks = () => {
                   </div>
                 </div>
                 <div className={`gap-3 !pt-0`}>
-                  <Table key={file.id}>
-                    <TableHeader>
-                      <TableRow>
-                        <TableHead>Category</TableHead>
-                        <TableHead>Description</TableHead>
-                        <TableHead>Amount</TableHead>
-                        <TableHead>Billable</TableHead>
-                        <TableHead>Customer</TableHead>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                      {file.data.lineItems.map((lineItem, lineItemIndex) => (
-                        <TableRow
-                          key={lineItemIndex + file.id + lineItem.productCode}
-                        >
-                          <TableCell>
-                            <ComboBox
-                              options={accounts}
-                              valueToMatch={lineItem.productCode || 'General'}
-                              callBackFunction={(value) =>
-                                handleAccountSelect(
-                                  value,
-                                  fileIndex,
-                                  lineItemIndex,
-                                )
-                              }
-                              getOptionLabel={(option) => option?.Name}
-                            />
-                          </TableCell>
-                          <TableCell>{lineItem.description}</TableCell>
-                          <TableCell className="text-right">
-                            ${parseFloat(lineItem.totalAmount)?.toFixed(2)}
-                          </TableCell>
-                          <TableCell>
-                            <Checkbox
-                              defaultChecked
-                              onCheckedChange={(value: boolean) =>
-                                handleBillableChange(
-                                  Boolean(value),
-                                  fileIndex,
-                                  lineItemIndex,
-                                )
-                              }
-                            />
-                          </TableCell>
-                          <TableCell>
-                            <ComboBox
-                              options={customers}
-                              valueToMatch={file.data.customerAddress}
-                              callBackFunction={(value) =>
-                                handleCustomerSelect(
-                                  value,
-                                  fileIndex,
-                                  lineItemIndex,
-                                )
-                              }
-                              getOptionLabel={(option) => option?.DisplayName}
-                            />
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                      <TableRow>
-                        <TableCell colSpan={3}>
-                          <div className="flex flex-row">
-                            <div className="flex w-3/4 flex-col">
-                              <div className="py-0 text-right font-medium">
-                                Sub-Total:
-                              </div>
-                              <div className="py-0 text-right font-medium">
-                                Tax:
-                              </div>
-                              <div className="py-0 text-right font-medium">
-                                Net Total:
-                              </div>
-                            </div>
-                            <div className="flex w-1/4 min-w-[100px] flex-col text-right">
-                              <div className="py-0">
-                                ${file.data.totalAmount.toFixed(2)}
-                              </div>
-                              <div className="py-0">
-                                {Number(file.data.totalTax)?.toFixed(2)}
-                              </div>
-                              <div className="py-0">
-                                ${file.data.totalNet.toFixed(2)}
-                              </div>
-                            </div>
-                          </div>
-                        </TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell colSpan={5}>
-                          <div className="flex flex-row justify-between w-full">
-                            <Button
-                              onClick={() => {
-                                setActiveIndex(fileIndex);
-                              }}
-                              variant={'outline'}
-                              disabled={activeIndex === fileIndex}
-                            >
-                              <EyeOpenIcon className="h-4 w-4" /> View Invoice
-                            </Button>
-                            <div className="flex flex-row gap-2">
-                              <Button
-                                variant={'ghost'}
-                                onClick={() =>
-                                  setUploadedFileIndexes([
-                                    ...uploadedFileIndexes,
-                                    fileIndex,
-                                  ])
-                                }
-                              >
-                                <BookmarkIcon /> Save as Draft
-                              </Button>
-                              <Button
-                                onClick={() => uploadToQuickBooks(fileIndex)}
-                                disabled={
-                                  isLoading ||
-                                  uploadedFileIndexes.includes(fileIndex) ||
-                                  file.status !== InvoiceStatus.APPROVED
-                                }
-                                variant={'secondary'}
-                              >
-                                <IfElseRender
-                                  condition={isLoading}
-                                  ifTrue={
-                                    <>
-                                      <Loader2Icon className="h-4 w-4 animate-spin" />{' '}
-                                      Submitting...
-                                    </>
-                                  }
-                                  ifFalse={
-                                    <>
-                                      <HammerIcon className="h-4 w-4" /> Submit
-                                    </>
-                                  }
-                                />
-                              </Button>
-                            </div>
-                          </div>
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
+                  <InvoiceDataTable
+                    file={file}
+                    fileIndex={fileIndex}
+                    transformedFiles={transformedFiles}
+                    setTransformedFiles={setTransformedFiles}
+                    uploadedFileIndexes={uploadedFileIndexes}
+                    setUploadedFileIndexes={setUploadedFileIndexes}
+                  />
                 </div>
               </div>
             </div>
