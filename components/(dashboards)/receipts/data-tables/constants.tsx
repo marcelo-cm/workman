f6abb47cece97f84a3cecab51196c9dc76d4bade
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-09-09 00:19:49 +0200

    changes

--- components/(dashboards)/receipts/data-tables/constants.tsx
+++ components/(dashboards)/receipts/data-tables/constants.tsx
@@ -2,50 +2,42 @@ import { ReactNode } from 'react';
 
 import { Check, Inbox, Scan } from 'lucide-react';
 
-import { InvoiceStatus, ReceiptStatus } from '@/constants/enums';
-import {
-  InvoiceCountResponseKeys,
-  ReceiptCountResponseKeys,
-} from '@/interfaces/db.interfaces';
+import { ReceiptStatus } from '@/constants/enums';
+import { ReceiptCountResponseKeys } from '@/interfaces/db.interfaces';
 import { User } from '@/models/User';
 
-export interface InvoiceTabValue {
-  state: InvoiceStatus | InvoiceStatus[];
-  approverId: string | null;
-}
-
-export const INVOICE_DATA_TABLE_TABS = (
+export const RECEIPT_DATA_TABLE_TABS = (
   user: User,
 ): {
   title: string;
   icon: ReactNode;
-  value: InvoiceTabValue;
-  countKey?: InvoiceCountResponseKeys;
+  value: ReceiptTabValue;
+  countKey?: ReceiptCountResponseKeys;
 }[] => {
   return [
     {
       title: 'Company Inbox',
       icon: <Inbox className="h-4 w-4" />,
       value: {
-        state: [InvoiceStatus.FOR_REVIEW, InvoiceStatus.APPROVED],
+        state: [ReceiptStatus.FOR_REVIEW, ReceiptStatus.APPROVED],
         approverId: null,
       },
-      countKey: InvoiceCountResponseKeys.COMPANY_INBOX,
+      countKey: ReceiptCountResponseKeys.COMPANY_INBOX,
     },
     {
       title: 'Awaiting Your Review',
       icon: <Inbox className="h-4 w-4" />,
       value: {
-        state: InvoiceStatus.FOR_REVIEW,
+        state: ReceiptStatus.FOR_REVIEW,
         approverId: user.id,
       },
-      countKey: InvoiceCountResponseKeys.AWAITING_REVIEW,
+      countKey: ReceiptCountResponseKeys.AWAITING_REVIEW,
     },
     {
       title: 'Awaiting Scan',
       icon: <Scan className="h-4 w-4" />,
       value: {
-        state: InvoiceStatus.UNPROCESSED,
+        state: ReceiptStatus.UNPROCESSED,
         approverId: null,
       },
     },
@@ -53,9 +45,14 @@ export const INVOICE_DATA_TABLE_TABS = (
       title: 'Completed',
       icon: <Check className="h-4 w-4" />,
       value: {
-        state: InvoiceStatus.PROCESSED,
+        state: ReceiptStatus.PROCESSED,
         approverId: null,
       },
     },
   ];
 };
+
+export interface ReceiptTabValue {
+  state: ReceiptStatus | ReceiptStatus[];
+  approverId: string | null;
+}
