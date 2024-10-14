9a634fd6d6a211ac705593db878c0a2cfe4d592c
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-10-14 18:35:21 +0200

    decode uri for invoice name

--- models/Invoice.ts
+++ models/Invoice.ts
@@ -388,7 +388,7 @@ export class Invoice {
   }
 
   get fileName(): string {
-    return this._file_url.split('/').pop()?.split('.pdf')[0] || '';
+    return decodeURI(this._file_url.split('/').pop()?.split('.pdf')[0] || '');
   }
 
   set data(data: InvoiceData) {
