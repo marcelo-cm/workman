54c25743b87f30c9f32c0fd093f477c69e14941d
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-10-24 00:13:31 +0200

    uses loading until the user is valid

--- app/(dashboards)/layout.tsx
+++ app/(dashboards)/layout.tsx
@@ -3,6 +3,8 @@
 import React, { useEffect, useState } from 'react';
 
 import SideBar from '@/components/(dashboards)/layout/SideBar';
+import WorkmanLogo from '@/components/molecules/WorkmanLogo';
+import IfElseRender from '@/components/ui/if-else-renderer';
 
 import { useUser } from '@/lib/hooks/supabase/useUser';
 
@@ -12,7 +14,7 @@ import { AppContext } from './context';
 
 const AppLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   const { fetchUserData } = useUser();
-  const [user, setUser] = useState<User>({} as User);
+  const [user, setUser] = useState<User>(null as unknown as User);
 
   useEffect(() => {
     refetchUser();
@@ -26,11 +28,17 @@ const AppLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     <div className="flex h-dvh w-dvw flex-row overflow-x-hidden bg-white text-black">
       <AppContext.Provider value={{ user, refetchUser }}>
         <SideBar />
-        {user.id && (
-          <main className="no-scrollbar flex h-dvh w-full items-center overflow-scroll">
-            {children}
-          </main>
-        )}
+        <IfElseRender
+          condition={!!user}
+          ifTrue={
+            <main className="no-scrollbar flex h-dvh w-full items-center overflow-scroll">
+              {children}
+            </main>
+          }
+          ifFalse={
+            <WorkmanLogo className="mx-auto my-auto w-64 animate-pulse " />
+          }
+        />
       </AppContext.Provider>
     </div>
   );
