b8604512cdc054a607a2216d52e4403c2c839be1
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-10-24 23:33:37 +0200

    deploy with saving payload

---
+++ app/api/v1/workman/kayott/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { ok } from '@/app/api/utils';
+import { createClient } from '@/lib/utils/supabase/server';
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  const supabase = createClient();
+  const data = req;
+
+  const response = await supabase.from('temp').insert({ payload: data });
+
+  return ok('Received');
+}
