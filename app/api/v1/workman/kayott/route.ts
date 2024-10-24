89a3d79661cc1491a6953e09d39b8c1645cde21f
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-10-24 23:43:49 +0200

    body

--- app/api/v1/workman/kayott/route.ts
+++ app/api/v1/workman/kayott/route.ts
@@ -5,7 +5,7 @@ import { createClient } from '@/lib/utils/supabase/server';
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   const supabase = createClient();
-  const data = req;
+  const data = req.body;
 
   const response = await supabase
     .from('temp')
